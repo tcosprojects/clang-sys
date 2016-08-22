ba0317a160d867d4784e02ea68ad590908e352e8
Kyle Mayes <kyle@mayeses.com>
2016-08-21 20:15:30 -0400

    "Update documentation"

---
+++ 3_6/implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libloading"] = [];implementors["clang_sys"] = [];implementors["clang_sys"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ 3_7/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["glob"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='glob/struct.GlobError.html' title='glob::GlobError'>GlobError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='glob/struct.PatternError.html' title='glob::PatternError'>PatternError</a>",];implementors["libloading"] = [];implementors["clang_sys"] = [];implementors["clang_sys"] = [];implementors["clang_sys"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
